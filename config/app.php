a777af258888564c6e1f88a43ca8ec8aa94a5b2e
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2017-04-24 15:38:11 -0300

    Add provider

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        BackupManager\Laravel\Laravel5ServiceProvider::class,
 
         /*
          * Application Service Providers...
